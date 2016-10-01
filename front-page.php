e0cc5b5435543379abd17eb61c7cd63302d5348d
JTylerH <john.tyler.hall@gmail.com>
2016-10-01 17:20:47 -0400

    init

---
+++ front-page.php
@@ -0,0 +1,99 @@
+<?php get_header(); ?>
+
+			<div id="front-page-header">
+				<div class="hero">
+					<div class="container">
+						<h1 class="tag">Stuart McCloud Music</h1>
+						<h2 class="subtag">Music Producer &amp; Composer</h2>
+					</div>
+				</div>
+			</div>
+			<main id="content">
+				<section class="offwhite">
+					<div class="container narrow">
+						<p class="text-center">
+							With a wide variety in styles from bluesy rock, 80s synth pop,
+							and R&amp;B to cinematic, ambient, and chill wave, Stuart McCloud
+							will work with whatever the client needs to fit the specific
+							project and feel they are going for.
+						</p>
+					</div>
+				</section>
+				<section id="works">
+					<div class="container">
+						<h2>Client Works</h2>
+					</div>
+					<div class="container projects">
+						<div class="row">
+							<div class="col-sm-6">
+								<div class="project">
+									<a href="#">
+										<img src="<?php echo get_template_directory_uri(); ?>/library/img/flagship.jpg">
+									</a>
+									<div class="caption">
+										<p><strong>Client:</strong> Flagship Visual Co.</p>
+										<p><strong>Role:</strong> Scored, Mixed, Mastered</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-sm-6">
+								<div class="project">
+									<a href="#">
+										<img src="<?php echo get_template_directory_uri(); ?>/library/img/ryltd.jpg">
+									</a>
+									<div class="caption">
+										<p><strong>Client:</strong> Flagship Visual Co.</p>
+										<p><strong>Role:</strong> Scored, Mixed, Mastered</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-sm-6">
+								<div class="project">
+									<a href="#">
+										<img src="<?php echo get_template_directory_uri(); ?>/library/img/couldever.jpg">
+									</a>
+									<div class="caption">
+										<p><strong>Client:</strong> Flagship Visual Co.</p>
+										<p><strong>Role:</strong> Scored, Mixed, Mastered</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-sm-6">
+								<div class="project">
+									<a href="#">
+										<img src="<?php echo get_template_directory_uri(); ?>/library/img/ztu.jpg">
+									</a>
+									<div class="caption">
+										<p><strong>Client:</strong> Flagship Visual Co.</p>
+										<p><strong>Role:</strong> Scored, Mixed, Mastered</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-sm-6">
+								<div class="project">
+									<a href="#">
+										<img src="<?php echo get_template_directory_uri(); ?>/library/img/collector.jpg">
+									</a>
+									<div class="caption">
+										<p><strong>Client:</strong> Flagship Visual Co.</p>
+										<p><strong>Role:</strong> Scored, Mixed, Mastered</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-sm-6">
+								<div class="project">
+									<a href="#">
+										<img src="<?php echo get_template_directory_uri(); ?>/library/img/parocco.jpg">
+									</a>
+									<div class="caption">
+										<p><strong>Client:</strong> Flagship Visual Co.</p>
+										<p><strong>Role:</strong> Scored, Mixed, Mastered</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+			</main>
+
+<?php get_footer(); ?>
